f339a27792f74590abcd88aaaea61122fa1dd0f7
vsheffer <vincent.sheffer@offerdox.com>
2022-08-16 23:28:29 -0700

    Added another test case for functions.

--- test/functions_test.dart
+++ test/functions_test.dart
@@ -15,21 +15,29 @@ Object? testFuncWithoutContext({String namedArg1 = 'default'}) {
 void main() {
   group('No Context', () {
     test('Named Argument', () {
-      var data = {'ignore': 'me'};
       var env = Environment(globals: {'test_func': testFuncWithoutContext});
       var out = env.fromString("{{ test_func(namedArg1='testing') }}");
-      expect(out.render(data), 'testing');
+      expect(out.render(), 'testing');
     });
   });
   group('With Context', () {
-    var data = {'bar': 42};
     test('Named Argument', () {
+      var data = {'bar': 42};
       var env = Environment(
         getAttribute: getAttribute,
         globals: {'test_func': passContext(testFuncWithContext)},
       );
       var out = env.fromString("{{ test_func(namedArg1='testing') }}");
       expect(out.render(data), 'testing42');
+    });
+    test('Named Argument Missing', () {
+      var data = {'bar': 42};
+      var env = Environment(
+        getAttribute: getAttribute,
+        globals: {'test_func': passContext(testFuncWithContext)},
+      );
+      var out = env.fromString("{{ test_func() }}");
+      expect(out.render(data), 'default42');
     });
   });
 }
